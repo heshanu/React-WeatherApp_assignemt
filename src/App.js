04d797c4ecf2ba96b8ed474378b7989325bcead4
heshanu <heshanu97@gmail.com>
2023-03-13 19:20:39 +0530

    chaged

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter, Link, Route, Routes, NavLink } from "react-router-dom";
 import "./App.css";
 
 import Geo from "./Component/weatherPage/geo";
-import Weather3days from "./template/weather3days";
-import Weatherweek from "./template/weatherweek";
-import Page404 from "./template/Page404";
+// import Weather3days from "./template/weather3days";
+// import Weatherweek from "./template/weatherweek";
+// import Page404 from "./template/Page404";
+
+import LoginButton from "./template/login";
+import LogoutButton from "./template/logout";
+
+const {
+  loginWithPopup,
+  loginWithRedirect,
+  logout,
+  user,
+  isAuthenticated,
+} = useAuth0();
 
 function App() {
   return (
     <>
       <h1>Welcome to WeatherApp!</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact component={Geo} />
-          <Route path="/weather3days" component={Weather3days} />
-          <Route path="/weatherweek" component={Weatherweek} />
-          <Route path="/*" component={Page404} />
-        </Routes>
-      </BrowserRouter>
-      <Geo />
-      <br />
-      <button>
-      </button>
+      <LoginButton />
+      <LogoutButton />
+      <Geo/>
     </>
   );
 }
