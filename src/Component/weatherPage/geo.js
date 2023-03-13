a9ecd7576c057bd60c5bda71690ad991d2e73f9c
heshanu <heshanu97@gmail.com>
2023-03-13 13:05:56 +0530

    updated

--- src/Component/weatherPage/geo.js
+++ src/Component/weatherPage/geo.js
@@ -1,17 +1,68 @@
-import React from "react";
-import cordinate from "./cordinate";
+import React, { useState, useEffect } from "react";
 
 const Geo = () => {
+  const [location, setLocation] = useState("");
+  const url =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+    location +
+    ",UK?key=" +
+    process.env.REACT_APP_WEATHER_API_KEY;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(location);
+    if (location) {
+      //const location = { location: location };
+      setLocation((l) => {
+        return [location];
+      });
+    } else {
+      alert("Please enter the location");
+    }
+  };
+
   var projectStyle = {
     width: 300,
     height: 300,
-    border: 10,backgroundColor: "blue",
+    border: 10,
+    backgroundColor: "blue",
   };
+
+  useEffect(
+    (url) => {
+      return () => {
+
+      };
+    },
+    [location]
+  );
   return (
-    <div style={projectStyle}>
-      <h1>Geo Locations</h1>
-      <cordinate />
-    </div>
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <button type="submit">Search Weather</button>
+        </form>
+        {/*location.map((person, index) => {
+          const { id } = person;
+          return (
+            <>
+              <div className="item" key={id}>
+                <h4></h4>
+              </div>
+            </>
+          );
+        })*/}
+      </article>
+    </>
   );
 };
 
