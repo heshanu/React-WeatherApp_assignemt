c97f1439674e5fa2a0dc9a05e9011613759d4c52
heshanu <heshanu97@gmail.com>
2023-03-13 16:19:59 +0530

    Added

--- src/Component/weatherPage/geo.js
+++ src/Component/weatherPage/geo.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
+import fetchData from "./fetchData";
+import Weather3days from "../../template/weather3days";
 
 const Geo = () => {
   const [location, setLocation] = useState("");
-  const url =
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
-    location +
-    ",UK?key=" +
-    process.env.REACT_APP_WEATHER_API_KEY;
+  const [latitude, setLatitude] = useState(6.9271);
+  const [longtitude, setLongtitude] = useState(79.8612);
+  const [data, setData] = useState([]);
+
+  // const url ="https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=latitude&lon=longtitude&appid=process.env.REACT_APP_WEATHER_API_KEY";
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(location);
     if (location) {
       //const location = { location: location };
+
       setLocation((l) => {
         return [location];
       });
+      setLatitude("");
+      setLongtitude("");
+    } else if (latitude && longtitude) {
+      setLatitude(latitude);
+      setLongtitude(longtitude);
+      console.log(latitude, longtitude);
     } else {
       alert("Please enter the location");
     }
@@ -27,14 +36,24 @@ const Geo = () => {
     backgroundColor: "blue",
   };
 
-  useEffect(
-    (url) => {
-      return () => {
+  useEffect(() => {
+    fetchData(latitude, longtitude);
+    // const fetchData = async () => {
+    //   // navigator.geolocation.getCurrentPosition(function (position) {
+    //   //   setLatitude(position.coords.latitude);
+    //   //   setLongtitude(position.coords.longitude);
+    //   // });
 
-      };
-    },
-    [location]
-  );
+    //   await fetch(url)
+    //     .then((res) => res.json())
+    //     .then((result) => {
+    //       setData(result);
+    //       console.log(result);
+    //     });
+    // };
+    fetchData(latitude, longtitude);
+  }, [latitude, longtitude]);
+
   return (
     <>
       <article>
@@ -49,16 +68,34 @@ const Geo = () => {
               onChange={(e) => setLocation(e.target.value)}
             />
           </div>
+          <div className="form-control">
+            <label htmlFor="lat">Latitude:</label>
+            <input
+              type="text"
+              id="Latitude"
+              name="Latitude"
+              value={latitude}
+              onChange={(e) => setLatitude(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="location">longitude:</label>
+            <input
+              type="text"
+              id="longtitude"
+              name="longtitude"
+              value={longtitude}
+              onChange={(e) => setLongtitude(e.target.value)}
+            />
+          </div>
           <button type="submit">Search Weather</button>
         </form>
-        {/*location.map((person, index) => {
-          const { id } = person;
+        <Weather3days data={fetchData(latitude, longtitude)} />
+        {/*data.map((index, d) => {
           return (
-            <>
-              <div className="item" key={id}>
-                <h4></h4>
-              </div>
-            </>
+            <div key={d} style={projectStyle}>
+              <h1>{}</h1>
+            </div>
           );
         })*/}
       </article>
