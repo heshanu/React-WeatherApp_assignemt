b7bb23e8835b3ea36456e961e89e8af4cb11273a
heshanu <heshanu97@gmail.com>
2023-03-13 12:27:43 +0530

    update corrdinates

--- src/Component/weatherPage/cordinate.js
+++ src/Component/weatherPage/cordinate.js
@@ -1,1 +1,6 @@
 import React from "react";
+const Cordinate = () => {
+  return <>cordinate</>;
+};
+
+export default Cordinate;
--- src/Component/weatherPage/geo.js
+++ src/Component/weatherPage/geo.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import cordinate from './cordinate';
 
 const Geo=()=>{
     return(
     <div>
         <h1>Geo Locations</h1>
-        
-
+        <cordinate />
     </div>)
 }
 
