e9f34b692c9251eb6e077071fae44810ae11d464
heshanu <heshanu97@gmail.com>
2023-03-13 16:35:00 +0530

    Adde

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
 import Geo from "./Component/weatherPage/geo";
-import { Weather3days } from "./template/weather3days";
+import Weather3days from "./template/weather3days";
 import Weatherweek from "./template/weatherweek";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to WeatherApp</h1>
-        <Geo />
-        <button>
-          <a target="_blank">show weekly weather</a>
-        </button>
-      </header>
-    </div>
+    <>
+      <h1>Welcome to WeatherApp!</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Geo} />
+          <Route path="/weather3days" component={Weather3days} />
+          <Route path="/weatherweek" component={Weatherweek} />
+        </Switch>
+      </Router>
+      <Geo />
+      <button>
+        <a href="/weather3days">3 days weather</a>
+      </button>
+    </>
   );
 }
 
--- src/template/weatherweek.js
+++ src/template/weatherweek.js
@@ -1,5 +1,5 @@
 import React from "react";
-import fetchData from "./fetchData";
+import fetchData from "../Component/weatherPage/fetchData";
 
 const Weatherweek = ({ data }) => {
   const { lan, log, location } = data;
