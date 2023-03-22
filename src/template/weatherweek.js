f3f2407ec102526b957af5ed5d8b6fbc33eb04b5
heshanu <heshanu97@gmail.com>
2023-03-22 22:09:25 +0530

    Add

--- src/template/weatherweek.js
+++ src/template/weatherweek.js
@@ -1,32 +1,12 @@
 import React from "react";
 
-<<<<<<< HEAD
-const Weatherweek = ({ data }) => {
-  return (
-    <>
-      <h1>Weather Week</h1>
-=======
 const Weatherweek = () => {
-  //const { lan, log, location } = data;
   return (
-    <>
-      <h1>Weather Week</h1>
-      
->>>>>>> b3596c39694251422ea3c658c43c78cdf0423ea0
-      {/*data &&
-        data.map((index, d) => {
-          return (
-            <>
-              <div>
-                <h1>{lan}</h1>
-                <h1>{log}</h1>
-                <h1>{location}</h1>
-              </div>
-            </>
-          );
-        })*/}
-    </>
+    <div>
+      <h1>Weatherweek</h1>
+    </div>
   );
+ 
 };
 
 export default Weatherweek;
